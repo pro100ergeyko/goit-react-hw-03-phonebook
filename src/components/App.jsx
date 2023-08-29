e2c42eee627b85d8267524fca4e3938d0cb5e8aa
pro100ergeyko <124717620+pro100ergeyko@users.noreply.github.com>
2023-08-29 21:51:50 +0300

    finished the first task for the third hw

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -21,6 +21,21 @@ export class App extends Component {
     filter: '',
   };
 
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
   handleSubmit = ({ name, number }, { resetForm }) => {
     const { contacts } = this.state;
 
